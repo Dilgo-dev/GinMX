4041a7b54787596f76b872d034c788dfbc91e818
Dilgo-dev <dilgopierre@gmail.com>
2025-05-22 05:35:38 +0200

    feat: Load environment variables and configure server port

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,28 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello, World!",
 		})
 	})
-	r.Run(":8080")
+	r.Run(":" + port)
 }
