98642abeaea70638063bfa0bdce6d7f125b62729
Dilgo-dev <dilgopierre@gmail.com>
2025-05-22 06:25:26 +0200

    feat: Implement database configuration and initialization using GORM for PostgreSQL

---
+++ internal/config/database.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Dilgo-dev/GinMX/internal/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_SSLMODE"))
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.AutoMigrate(&models.Blog{})
+	DB = db
+}
+
+func GetDB() *gorm.DB {
+	return DB
+}
